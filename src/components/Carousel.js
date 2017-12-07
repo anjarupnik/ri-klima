f11224442462c254dd56e589e3136da9cf73ac8e
anjarupnik <anjarupnik@hotmail.com>
2017-12-07 14:43:52 +0100

    paper as seperate componenet

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,8 @@
 import React, { Component } from 'react';
 import Carousel from 'nuka-carousel'
-import Paper from 'material-ui/Paper'
 import './Carousel.css'
 import { Decorators } from './Decorators'
-import RaisedButton from 'material-ui/RaisedButton';
-import Ac from 'material-ui/svg-icons/places/ac-unit';
-
-const style = {
-  button: {
-   margin: 12,
- },
- exampleImageInput: {
-   cursor: 'pointer',
-   position: 'absolute',
-   top: 0,
-   bottom: 0,
-   right: 0,
-   left: 0,
-   width: '100%',
-   opacity: 0,
- },
-  height: '120%',
-  width: '80%',
-  margin: 20,
-  display: 'flex',
-  justifyContent: 'space-around',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-}
-
-
+import Paper from './Paper'
 
 class Caro extends Component {
   render() {
@@ -38,28 +11,15 @@ class Caro extends Component {
         <Carousel
           cellAlign='left'
           decorators={Decorators}
+          autoplay={true}
           >
-          <div className="slide">
-            <Paper style={style} zDepth={4} rounded={false}>
-              <img className="img" src="http://res.cloudinary.com/mdfchucknorris/image/upload/v1512651883/ke-tke_enqaew.png" alt="klima"/>
-              <div className="info">
-                <h3 className="model">3,5 kW: GWH12RA</h3>
-                <h4 className="price">Cijena: 3.750,00 kn</h4>
-                <RaisedButton
-                   label="Opširnije"
-                   labelPosition="before"
-                   primary={true}
-                   icon={<Ac />}
-                   style={style.button}
-                 />
-              </div>
-            </Paper>
-          </div>
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"/>
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"/>
+          <Paper />
+          <Paper />
+          <Paper />
+          <Paper />
+          <Paper />
+          <Paper />
+          <Paper />
         </Carousel>
       </div>
     )
---
+++ src/components/Paper.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Ac from 'material-ui/svg-icons/places/ac-unit';
+import Paper from 'material-ui/Paper'
+import './Carousel.css'
+
+const style = {
+  button: {
+   margin: 12,
+ },
+ exampleImageInput: {
+   cursor: 'pointer',
+   position: 'absolute',
+   top: 0,
+   bottom: 0,
+   right: 0,
+   left: 0,
+   width: '100%',
+   opacity: 0,
+ },
+  height: '120%',
+  width: '80%',
+  margin: 20,
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+}
+
+class ACPaper extends PureComponent {
+  render() {
+    return (
+      <div className="slide">
+        <Paper style={style} zDepth={4} rounded={false}>
+          <img className="img" src="http://res.cloudinary.com/mdfchucknorris/image/upload/v1512651883/ke-tke_enqaew.png" alt="klima"/>
+          <div className="info">
+            <h3 className="model">3,5 kW: GWH12RA</h3>
+            <h4 className="price">Cijena: 3.750,00 kn</h4>
+            <RaisedButton
+               label="Opširnije"
+               labelPosition="before"
+               primary={true}
+               icon={<Ac />}
+               style={style.button}
+             />
+          </div>
+        </Paper>
+      </div>
+    )
+  }
+}
+
+export default ACPaper
