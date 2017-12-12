376849329228d35e256807b33cd6212a663c276b
anjarupnik <anjarupnik@hotmail.com>
2017-12-12 10:54:37 +0100

    contact page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import Home from './containers/Home'
+import { Home, Kontakt } from './containers'
 
 export default class Routes extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/"  component={Home} />
+        <Route path="/kontakt" component={Kontakt} />
       </div>
     )
   }
